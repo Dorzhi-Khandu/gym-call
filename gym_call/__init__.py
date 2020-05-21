9c39a8bc456d3e77bd44857e53272f635f5fc187
Khandu <dorzhikhandu@mail.ru>
2020-05-21 13:04:28 +0300

    change to py format

--- gym_call/__init__.py
+++ gym_call/__init__.py
@@ -1,43 +1,10 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "from gym.envs.registration import register\n",
-    "\n",
-    "register(\n",
-    "    id='call-v0',\n",
-    "    entry_point='gym_call.envs:CallEnv',\n",
-    ")\n",
-    "register(\n",
-    "    id='call-extrahard-v0',\n",
-    "    entry_point='gym_call.envs:CallExtraHardEnv',\n",
-    ")"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.6.6"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+from gym.envs.registration import register
+
+register(
+    id='call-v0',
+    entry_point='gym_call.envs:CallEnv',
+)
+register(
+    id='call-extrahard-v0',
+    entry_point='gym_call.envs:CallExtraHardEnv',
+)
--- setup.py
+++ setup.py
@@ -1,39 +1,6 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "from setuptools import setup\n",
-    "\n",
-    "setup(name='gym_call',\n",
-    "      version='0.0.1',\n",
-    "      install_requires=['gym']  # And any other dependencies foo needs\n",
-    ")"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.6.6"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+from setuptools import setup
+
+setup(name='gym_call',
+      version='0.0.1',
+      install_requires=['gym']  # And any other dependencies foo needs
+)
